0966dbbe9b1cd6584b6c043b69d4a3721828e940
Double1V <113297720+Double1V@users.noreply.github.com>
2022-09-23 14:13:33 +0300

    GrassHopper

---
+++ GrassHopper.java
@@ -0,0 +1,10 @@
+public class GrassHopper {
+
+    public static int summation(int n) {
+        int s=0;
+        for (int i=0; i<=n; ++i){
+            s=s+i;
+        }
+        return s;
+    }
+}
